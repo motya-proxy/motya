6e9f84794644fa96889cf360441050a5aa4ad6be
Tam1SH <ignatternyev54@gmail.com>
2025-12-15 23:47:25 +0300

    feat: add Dependabot configuration and update CI workflow

--- ci.sh
+++ ci.sh
@@ -8,13 +8,6 @@ cargo fmt --all -- --check
 # Test all crates
 cargo test --all
 
-# Run configuration file checks
-# cd ./source/motya
-# cargo run -p motya -- --config-toml ./assets/example-config.toml --validate-configs
-# cargo run -p motya -- --config-toml ./assets/test-config.toml --validate-configs
-# cargo run -p motya -- --config-kdl ./assets/test-config.kdl --validate-configs
-# cd ../../
-
 # ensure the user manual can be built
 cd user-manual
 mdbook build
